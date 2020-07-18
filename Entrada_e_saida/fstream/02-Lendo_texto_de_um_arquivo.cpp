2089d329e87de2aea1352904f8a0e4ba7e5588ea
Francinildo-Figueiredo <66557567+Francinildo-Figueiredo@users.noreply.github.com>
2020-07-18 17:40:24 -0300

    up

--- Entrada_e_saida/fstream/02-Lendo_texto_de_um_arquivo.cpp
+++ Entrada_e_saida/fstream/02-Lendo_texto_de_um_arquivo.cpp
@@ -12,9 +12,11 @@ int main()
 {
 	setlocale( LC_ALL, "portuguese" );
 	
+	// Criando uma variável ifstream para leitura do arquivo.
 	ifstream arquivo("Exemplo1.txt");
 	char crt;
 	
+	// Enquanto tiver texto continue imprimindo.
 	while(arquivo.get(crt))
 		cout << crt;
 	
